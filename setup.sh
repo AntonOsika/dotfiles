7572242c1c57153eed1fbd2f58657eb85c655db0
Anton Osika <anton.osika@gmail.com>
2017-07-30 06:24:38 +0200

    replaced ~ and ~/dotfiles with $HOME and $PWD

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-dir=~/dotfiles
-
 FILES=".vimrc
 .zshrc
 .gitconfig
@@ -10,13 +8,13 @@ FILES=".vimrc
 
 for f in $FILES
 do
-	read -p "Do you want to symlink $f from home dir? Type [y] to OVERWRITE your old." -n 1 -r
+	read -p "Do you want to symlink $PWD/$f from home dir? Type [y] to OVERWRITE your old." -n 1 -r
 	echo    # move to a new line
 	if [[ $REPLY =~ ^[Yy]$ ]]
 	then
 		echo Backing up $f and then overwriting.
 		mv $HOME/$f $HOME/${f}BACKUP
-		ln -s $dir/$f ~/$f
+		ln -s $PWD/$f $HOME/$f
 	fi
 done
 
